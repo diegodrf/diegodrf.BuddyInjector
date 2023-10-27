2a177bd1098e2e4bbf09619ef50bfab13f84a811
Diego Faria <diego.rdfaria@gmail.com>
2023-10-27 09:33:31 -0400

    A little project to use as an example.

---
+++ diegodrf.BuddyInjector.ExampleProject/Models/Post.cs
@@ -0,0 +1,7 @@
+﻿namespace diegodrf.BuddyInjector.ExampleProject.Models;
+
+public record Post(
+    int Id, 
+    string Title, 
+    string Body, 
+    int UserId);
---
+++ diegodrf.BuddyInjector.ExampleProject/Services/IJsonPlaceHolderService.cs
@@ -0,0 +1,8 @@
+﻿using diegodrf.BuddyInjector.ExampleProject.Models;
+
+namespace diegodrf.BuddyInjector.ExampleProject.Services;
+
+public interface IJsonPlaceHolderService
+{
+    Task<Post?> GetPostByIdAsync(int id);
+}
