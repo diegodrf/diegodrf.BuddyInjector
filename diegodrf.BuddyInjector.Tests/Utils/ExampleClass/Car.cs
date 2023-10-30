ab7ba5ca7642aebf97f5ff45c6d39ddcfb0da66c
Diego Faria <diego.rdfaria@gmail.com>
2023-10-30 11:04:13 -0400

    Class for test multiple constructors exception

---
+++ diegodrf.BuddyInjector.Tests/Utils/ExampleClass/Car.cs
@@ -0,0 +1,16 @@
+﻿namespace diegodrf.BuddyInjector.Tests.Utils.ExampleClass;
+
+public class Car
+{
+    public string Name { get; set; }
+
+    public Car()
+    {
+        Name = string.Empty;
+    }
+
+    public Car(string name)
+    {
+        Name = name;
+    }
+}
