ea464b345f94c8d82649e2aea0d1058eb17ff379
Diego Faria <diego.rdfaria@gmail.com>
2023-10-30 11:02:14 -0400

    Add exception to represent new error

---
+++ diegodrf.BuddyInjector/Exceptions/MultipleConstructorsException.cs
@@ -0,0 +1,8 @@
+﻿using System.Text;
+
+namespace diegodrf.BuddyInjector.Exceptions;
+
+public class MultipleConstructorsException : Exception
+{
+    public MultipleConstructorsException(string? message = null) : base(message) { }
+}
