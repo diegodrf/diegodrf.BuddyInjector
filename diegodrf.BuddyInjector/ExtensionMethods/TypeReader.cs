eba2872f183610de3842b969faac12d0e64d001f
Diego Faria <diego.rdfaria@gmail.com>
2023-11-02 03:34:36 -0400

    Add new extension method to parse names

---
+++ diegodrf.BuddyInjector/ExtensionMethods/TypeReader.cs
@@ -0,0 +1,12 @@
+﻿namespace diegodrf.BuddyInjector.ExtensionMethods;
+
+public static class TypeReader
+{
+    public static string GetFormattedTypeName(this Type t)
+    {
+        if (!t.GenericTypeArguments.Any()) return t.Name;
+
+        var genericTypes = t.GenericTypeArguments.Select(x => x.Name);
+        return $"{t.Name}<{string.Join(", ", genericTypes)}>";
+    }
+}
