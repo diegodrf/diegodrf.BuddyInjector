91662d52523607aa52c9bb513357a3968294a8bf
Diego Faria <diego.rdfaria@gmail.com>
2024-02-06 16:40:12 -0500

    Implementing isDisposable validation

--- diegodrf.BuddyInjector/InstanceManager.cs
+++ diegodrf.BuddyInjector/InstanceManager.cs
@@ -5,6 +5,9 @@ public class InstanceManager
     private object? _instance;
     private readonly Func<object> _instanceBuilder;
     private readonly bool _isSingleton;
+    private bool _isDisposable;
+    public bool IsDisposable => _isDisposable;
+    
     
     public InstanceManager(Func<object> instanceBuilder, bool isSingleton = false)
     {
@@ -15,7 +18,13 @@ public class InstanceManager
     public object GetInstance()
     {
         if (!_isSingleton) return _instanceBuilder.Invoke();
-        _instance ??= _instanceBuilder.Invoke();
+        
+        if (_instance is null)
+        {
+            _instance = _instanceBuilder.Invoke();
+            _isDisposable = _instance is IDisposable;
+        }
+        
         return _instance;
     }
 }
