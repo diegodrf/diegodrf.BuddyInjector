bd340d9538da6b4c2c4dd1c4afc174f03d0ec59f
Diego Faria <diego.rdfaria@gmail.com>
2024-02-06 16:40:38 -0500

    Unit testing isDisposable

--- diegodrf.BuddyInjector.Tests/InstanceManagerTests.cs
+++ diegodrf.BuddyInjector.Tests/InstanceManagerTests.cs
@@ -29,4 +29,56 @@ public class InstanceManagerTests
         // Assert
         Assert.NotEqual(a, b);
     }
+
+    [Fact]
+    public void Given_A_Disposable_Class_Should_Set_IsDisposable_When_The_Object_Is_Singleton()
+    {
+        // Arrange
+        var sut = new InstanceManager(() => new DisposableClass(), true);
+        
+        // Act
+        sut.GetInstance();
+        
+        // Assert
+        Assert.True(sut.IsDisposable);
+    }
+    
+    [Fact]
+    public void Given_A_Disposable_Class_Should_Not_Set_IsDisposable_When_The_Object_Is_Transient()
+    {
+        // Arrange
+        var sut = new InstanceManager(() => new DisposableClass(), false);
+        
+        // Act
+        sut.GetInstance();
+        
+        // Assert
+        Assert.False(sut.IsDisposable);
+    }
+    
+    [Fact]
+    public void Given_A_Non_Disposable_Class_Should_Not_Set_IsDisposable_When_The_Object_Is_Singleton()
+    {
+        // Arrange
+        var sut = new InstanceManager(() => new Foo(), true);
+        
+        // Act
+        sut.GetInstance();
+        
+        // Assert
+        Assert.False(sut.IsDisposable);
+    }
+    
+    [Fact]
+    public void Given_A_Non_Disposable_Class_Should_Not_Set_IsDisposable_When_The_Object_Is_Transient()
+    {
+        // Arrange
+        var sut = new InstanceManager(() => new Foo(), false);
+        
+        // Act
+        sut.GetInstance();
+        
+        // Assert
+        Assert.False(sut.IsDisposable);
+    }
 }
---
+++ diegodrf.BuddyInjector.Tests/Utils/ExampleClass/DisposableClass.cs
@@ -0,0 +1,9 @@
+﻿namespace diegodrf.BuddyInjector.Tests.Utils.ExampleClass;
+
+public class DisposableClass: IDisposable
+{
+    public void Dispose()
+    {
+        // TODO release managed resources here
+    }
+}
